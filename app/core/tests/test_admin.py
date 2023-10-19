4aa94269e112535fec947e7ccb4893c3877ae66b
navyada <koshatwa@usc.edu>
2023-10-19 12:33:34 -0400

    lesson 12

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -14,7 +14,7 @@ class AdminSiteTests(TestCase):
 
     def setUp(self):
         """Create user and client"""
-        self.client = Client
+        self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='testpass123'
@@ -26,10 +26,23 @@ class AdminSiteTests(TestCase):
             name= 'Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """ Test the user edit page works"""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """ Test the user create page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
